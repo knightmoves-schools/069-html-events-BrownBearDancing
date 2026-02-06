ded3e078e8956a8af9ddad956bf38c684088c3b0
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2026-02-06 13:52:39 -0600

    see if what I have so far works

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ function calculateSquare(){
     let value = document.getElementById('number').value;
     let result = document.getElementById('result');
 
-    //add code here
-    
+    let square = (a){
+    return a * a
+        
+    }
 }
+document.getElementById('result').innerHTML = square;
+
+should multiply the input with the id number with itself and display the squared value in the div with the id result
+should display 'Invalid, please enter a number' in the div with the id result when the input is not a number.
