4d70c020130870a192fb4331c86b7574302378ff
BrownBearDancing <143032898+BrownBearDancing@users.noreply.github.com>
2026-02-17 13:58:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function calculateSquare(){
     let result = document.getElementById('result');
 if(isNaN(value) || value === '') {
     result.innerHTML = 'Invalid, please enter a number';
-}
-    else {
+}else {
         result.innerHTML = value * value;
     };
-    
+}
 document.getElementById('caluculate').addEventListener('click', calculateSquare;
 
 //should multiply the input with the id number with itself and display the squared value in the div with the id result
 //should display 'Invalid, please enter a number' in the div with the id result when the input is not a number.
+
 
 
 
